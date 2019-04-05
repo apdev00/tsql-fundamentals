02809ba2eb417492ee39735dd68f5f8a61399e5d
Alex Pasieka <alex.pasieka00@gmail.com>
2019-04-04 20:08:43 -0400

    chap04 20190404b

--- tsql-fundamentals-chap04.sql
+++ tsql-fundamentals-chap04.sql
@@ -12,3 +12,30 @@ WHERE
 	 WHERE o2.custid = o1.custid)
 ORDER BY
 	o1.custid;
+
+-------------------------------
+
+SELECT
+	o1.orderid
+,	o1.custid
+,	o1.val
+,	[pct] = CAST(100.00 * o1.val / (SELECT SUM(o2.val)
+									FROM Sales.OrderValues o2
+									WHERE o2.custid = o1.custid) AS NUMERIC(5,2))
+FROM
+	Sales.OrderValues o1
+ORDER BY
+	o1.custid, o1.orderid;
+
+--------------------------------
+
+SELECT
+	o1.orderid
+,	o1.orderdate
+,	o1.empid
+,	o1.custid
+,	[prevorderid] =	(SELECT MAX(o2.orderid)
+					 FROM Sales.Orders o2
+					 WHERE o2.orderid < o1.orderid)
+FROM
+	Sales.Orders o1;

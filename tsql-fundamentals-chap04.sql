cc0d8582c489ee35e312848f4299406008516e87
Alex Pasieka <alex.pasieka00@gmail.com>
2019-04-09 15:52:11 -0400

    chap04 20190409a

--- tsql-fundamentals-chap04.sql
+++ tsql-fundamentals-chap04.sql
@@ -39,3 +39,52 @@ SELECT
 					 WHERE o2.orderid < o1.orderid)
 FROM
 	Sales.Orders o1;
+
+--------------------------------
+
+SELECT * FROM Sales.OrderTotalsByYear;
+
+SELECT
+	o1.orderyear, o1.qty,  (SELECT SUM(o2.qty)
+							FROM Sales.OrderTotalsByYear o2
+							WHERE o2.orderyear <= o1.orderyear) AS runqty
+FROM
+	Sales.OrderTotalsByYear o1
+ORDER BY
+	o1.orderyear;
+
+--------------------------------
+
+DROP TABLE IF EXISTS Sales.MyShippers;
+
+CREATE TABLE Sales.MyShippers
+(
+	shipper_id	INT	NOT NULL,
+	companyname NVARCHAR(40) NOT NULL,
+	phone		NVARCHAR(24) NOT NULL,
+	CONSTRAINT PK_MyShippers PRIMARY KEY(shipper_id)
+);
+
+INSERT INTO Sales.MyShippers
+(
+    shipper_id,
+    companyname,
+    phone
+)
+VALUES
+(1, N'Shipper GVSUA', N'(503) 555-0137'),
+(2, N'Shipper ETYNR', N'(425) 555-0136'),
+(3, N'Shipper ZHISN', N'(415) 555-0138');
+
+SELECT * FROM Sales.MyShippers;
+SELECT * FROM Sales.Orders WHERE custid = 43
+
+SELECT *
+FROM Sales.MyShippers
+WHERE shipper_id IN (SELECT s.shipper_id FROM Sales.Orders s WHERE s.custid = 43);
+
+--------------------------------------------------------------------------------------------------------------
+-- Exercises: Chap 04
+--------------------------------------------------------------------------------------------------------------
+-- 1.
+SELECT * FROM Sales.Orders;
